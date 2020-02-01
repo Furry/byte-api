42f8c93755a7d1928f74087e75a801db9f698b53
A Furry Account <ender@thefluegges.com>
2020-01-31 19:50:08 -0500

    Moved http functions to CLIENT and added different post queries.
    Added:
    <client>.getPopularv2()
    <client>.getPopularv3()
    <client>.getMix() || ALIAS: <client>.getLatest()

--- src/constructors/http.js
+++ src/constructors/http.js
@@ -81,7 +81,6 @@ class HttpHandler extends EventEmitter {
         let result = []
 
         if (resulttype == "json") {
-            console.log("RAW")
             return response
         }
 
@@ -141,146 +140,13 @@ class HttpHandler extends EventEmitter {
 
     }
 
-    /**
-     * Sets your profile color
-     * @param {number} scheme The color you want your profile as. 1 to 17
-     * @returns {Promise<object>} the HTTP response.
-     * 
-     * @example
-     * setProfileColor(scheme)
-     * .then((res) => console.log(res))
-     */
-    setProfileColor(scheme) {
-        return this.baseRequest("account/me", "PUT", "profile", {"colorScheme": scheme})
-    }
-
-    /**
-     * Sets your Bio
-     * @param {string} message What you want your new BIO as.
-     * @returns {promise<object>} the HTTP response.
-     * 
-     * @example
-     * setBio("the one and only hackerman ;)")
-     * .then((res) => console.log(res))
-     */
-    setBio(message) {
-        return this.baseRequest("account/me", "PUT", "profile", {"bio": message})
-    }
-
-    /**
-     * Subscribes to a user
-     * @param {string} id User's ID to subscribe to.
-     * @returns {promise<object>} the HTTP response.
-     * 
-     * @example
-     * subscribe()
-     * .then((res) => console.log(res))
-     */
-    subscribe(id) {
-        console.log("<client>.subscribe() is DEPRECIATED. Please use <user>.subscribe, or <post>.author.subscribe()")
-        return this.baseRequest(`account/id/${id}/follow`, "PUT", "user")
-    }
-
-    /**
-     * Returns all posts in your global feed.
-     * @returns {array<posts>}
-     * 
-     * @example
-     * getGlobalFeed()
-     * .then((res) => res.forEach((post) => post.like()))
-     */
-    getGlobalFeed() {
-        return this.baseRequest("feed/global", "GET", "postuser")
-    }
-
-    /**
-     * Searches by name, and returns an array of User objects.
-     * @param {string} name The name you want to search.
-     * @returns {promise<array>}
-     * 
-     * @example
-     * searchByName("cat")
-     * .then((res) => {
-     * res.forEach((item) => console.log(item.id))
-     * })
-     */
-    searchByName(name) {
-        return this.baseRequest(`account/prefix/${name}`, "GET", "user")
-    }
-
-    /**
-     * To post a new video to the Client.
-     * @param {videoUrlResolvable} uri The resolvable URL of the video
-     * @param {string} [category="chill"] The category to post to
-     * @param {string} [caption=""] The caption
-     * @returns {post} The post you just created.
-     * @example
-     * .createPost("https://www.mp4url.com/mp4")
-     * .then((res) => console.log(res.videoSrc))
-     * 
-     * @example
-     * .createPost("./file.mp4")
-     * .then((res) => console.log(res.videoSrc))
-     */
-    createPost(uri, caption="", category) {
-        return new Promise(async (resolve, reject) => {
-            if (caption) caption = caption.toLowerCase()
-            let firstReq = await this.baseRequest("upload", "POST", "json", { "contentType": "video/mp4" })
-            .catch((err) => reject(err))
-
-            let buffer = await resolveSource.resolveBufferFromURL(uri)
-            .catch((err) => reject(err))
-
-            let secondReq = await this.baseRequest(firstReq.data.uploadURL, "PUT", "raw", { headers:{"Content-Type": "video/mp4"}, body: buffer}, { override: true })
-            .catch((err) => reject(err))
-
-            //let thirdReq = await this.baseRequest("post", "POST", "post", { category: category, videoUploadID: firstReq.data.uploadID, thumbUploadID: firstReq.data.uploadID, caption: caption})
-            //.catch((err) => reject(err))
-            //.then((res) => resolve(res))
-
-            this.baseRequest('post', 'POST', 'post', {
-                category: category,
-                videoUploadID: firstReq.data.uploadID,
-                thumbUploadID: firstReq.data.uploadID,
-                caption: caption
+    longPoll() {
+        return new Promise((resolve, reject) => {
+            this.baseRequest("activity", "GET", "json")
+            .catch((err) => {throw err})
+            .then((res) => {
+                return resolve(res)
             })
-            .catch((err) => reject(err))
-            .then((res) => resolve(res))
-            //console.log(await secondReq.body
-        })
-
-    }
-
-    /**
-     * To change the Client's account avatar.
-     * @param {imageUrlResolvable} uri The resolvable URL of the image
-     * @returns {promise<object>}
-     * 
-     * @example
-     * .setAvatar("https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png")
-     * .then((res) => console.log(res))
-     * 
-     * @example
-     * .setAvatar("./image.jpeg")
-     * .then((res) => console.log(res))
-     */
-    setAvatar(uri) {
-        return new Promise(async (resolve, reject) => {
-  
-            let firstReq = await this.baseRequest("upload", "POST", "json", { "contentType": "image/jpeg" })
-            .catch((err) => reject(err))
-  
-            let buffer = await resolveSource.resolveBufferFromURL(uri)
-            .catch((err) => reject(err))
-  
-            let secondReq = await this.baseRequest(firstReq.data.uploadURL, "PUT", "raw", { headers:{"Content-Type": "image/jpeg"}, body: buffer}, { override: true })
-            .catch((err) => reject(err))
-  
-            this.baseRequest('account/me', 'PUT', 'json', { avatarUploadID: firstReq.data.uploadID })
-            .catch((err) => reject(err))
-            .then((res) => resolve(res))
-  
-  
         })
     }
 
